0028af159ac2543cd89e124b8874dc673ddcf4f9
hermananenko <hermananenko@gmail.com>
2020-09-29 14:00:23 +0300

    Add makefile and Ninja build script

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "calculator.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	Calculator calc;
+	cout<<calc.Add(333.0, 444.0)<<endl;
+
+	return 0;
+}
+
